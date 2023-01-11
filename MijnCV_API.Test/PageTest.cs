7019be49669781725fd626522e25c06d8190650d
davey2206 <davey2206@live.nl>
2023-01-11 13:03:41 +0100

    switched tests to mocked data instead of fake services

--- MijnCV_API.Test/PageTest.cs
+++ MijnCV_API.Test/PageTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,114 +8,117 @@ using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using MijnCV_API.Controllers;
 using MijnCV_API.Models;
-using MijnCV_API.Test.Services;
+using MijnCV_API.Services;
 
 namespace MijnCV_API.Test
 {
     public class PageTest
     {
-        private readonly PagesController _controller;
-        private readonly PageServiceFake _service;
+        private readonly Mock<IPageService> _service;
         public PageTest()
         {
-            _service = new PageServiceFake();
-            _controller = new PagesController(_service);
+            _service = new Mock<IPageService>();
         }
 
         [Fact]
         public void GetAllPagesTest()
         {
-            var expected = new List<Page>()
-            {
-                new Page() { Id = 1, cv = "1", Name="Main" },
-                new Page() { Id = 2, cv = "1", Name="Second" },
-                new Page() { Id = 3, cv = "2", Name="Main" },
-                new Page() { Id = 4, cv = "2", Name="Second" },
-            };
-            var Result = _controller.GetPages();
+            var data = GetPagesData();
+            _service.Setup(x => x.GetPages()).Returns(async () => data);
+            var controller = new PagesController(_service.Object);
+
+            var Result = controller.GetPages();
 
             Assert.NotNull(Result);
             Assert.IsType<Task<ActionResult<IEnumerable<Page>>>>(Result);
-            Result.Result.Value.Should().Equals(expected);
+            Result.Result.Value.Should().Equals(data);
         }
 
         [Fact]
         public void GetPagesTest()
         {
-            Page expected = new Page() { Id = 1, cv = "1", Name = "Main" };
-            var Result = _controller.GetPage(1);
+            var data = GetPagesData();
+            _service.Setup(x => x.GetPage(1)).Returns(async () => data[0]);
+            var controller = new PagesController(_service.Object);
+
+            var Result = controller.GetPage(1);
 
             Assert.NotNull(Result);
             Assert.IsType<Task<ActionResult<Page>>>(Result);
-            Result.Result.Value.Should().Equals(expected);
+            Result.Result.Value.Should().Equals(data[0]);
         }
 
         [Fact]
         public void DeletePageTest()
         {
-            var expected = new List<Page>()
-            {
-                new Page() { Id = 2, cv = "1", Name="Second" },
-                new Page() { Id = 3, cv = "2", Name="Main" },
-                new Page() { Id = 4, cv = "2", Name="Second" },
-            };
+            var data = GetPagesData();
+            _service.Setup(x => x.DeletePage(1)).Returns(async () => true);
+            var controller = new PagesController(_service.Object);
 
-            var result = _service.DeletePage(1);
-            var test = _controller.GetPages();
+            var Result = controller.DeletePage(1);
+            var Result2 = controller.GetPages();
 
-            Assert.IsType<Task<bool>>(result);
-            test.Result.Value.Should().Equals(expected);
+            Assert.NotNull(Result);
+            Result2.Result.Value.Should().Equals(data);
         }
 
         [Fact]
         public void PutPageTest()
         {
-            Page expected = new Page() { Id = 1, cv = "1", Name = "Main2" };
             Page page = new Page() { cv = "1", Name = "Main2" };
 
-            var Result = _service.PutPage(1, page);
-            var Result2 = _controller.GetPage(1);
+            var data = GetPagesData();
+            _service.Setup(x => x.PutPage(1, page)).Returns(async () => true);
+            var controller = new PagesController(_service.Object);
+
+            var Result = controller.PutPage(1, page);
+            var Result2 = controller.GetPage(1);
 
             Assert.NotNull(Result);
-            Assert.IsType<Task<bool>>(Result);
-            Result2.Result.Value.Should().Equals(expected);
+            Result2.Result.Value.Should().Equals(data[0]);
         }
 
         [Fact]
         public void PostPageTest()
         {
-            var expected = new List<Page>()
-            {
-                new Page() { Id = 1, cv = "1", Name="Main" },
-                new Page() { Id = 2, cv = "1", Name="Second" },
-                new Page() { Id = 3, cv = "2", Name="Main" },
-                new Page() { Id = 4, cv = "2", Name="Second" },
-                new Page() { Id = 5, cv = "3", Name="Main" },
-            };
-
             Page page = new Page() { cv = "3", Name = "Main" };
 
-            var result = _service.PostPage(page);
-            var test = _controller.GetPages();
+            var data = GetPagesData();
+            _service.Setup(x => x.PostPage(page)).Returns(async () => data);
+            var controller = new PagesController(_service.Object);
 
-            Assert.IsType<Task<Page>>(result);
-            test.Result.Value.Should().Equals(expected);
+            var Result = controller.PostPage(page);
+            var Result2 = controller.GetPages();
+
+            Assert.NotNull(Result);
+            Assert.IsType<Task<ActionResult<Page>>>(Result);
+            Result2.Result.Value.Should().Equals(data);
         }
 
         [Fact]
         public void GetPagesByCvTest()
         {
-            var expected = new List<Page>()
-            {
-                new Page() { Id = 1, cv = "1", Name="Main" },
-                new Page() { Id = 2, cv = "1", Name="Second" },
-            };
+            var data = GetPagesData();
+            _service.Setup(x => x.GetPagesByCV("1")).Returns(async () => data);
+            var controller = new PagesController(_service.Object);
 
-            var Result = _controller.GetPagesByCV("1");
+            var Result = controller.GetPagesByCV("1");
 
             Assert.NotNull(Result);
             Assert.IsType<Task<ActionResult<IEnumerable<Page>>>>(Result);
-            Result.Result.Value.Should().Equals(expected);
+            Result.Result.Value.Should().Equals(data);
+        }
+
+        public List<Page> GetPagesData()
+        {
+            List<Page> pages = new List<Page>()
+            {
+                new Page() { Id = 1, cv = "1", Name="Main" },
+                new Page() { Id = 2, cv = "1", Name="Second" },
+                new Page() { Id = 3, cv = "2", Name="Main" },
+                new Page() { Id = 4, cv = "2", Name="Second" },
+            };
+            return pages;
         }
     }
 }
--- MijnCV_API.Test/SectionTest.cs
+++ MijnCV_API.Test/SectionTest.cs
@@ -1,8 +1,10 @@
 ﻿using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using MijnCV_API.Controllers;
 using MijnCV_API.Models;
-using MijnCV_API.Test.Services;
+using MijnCV_API.Services;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,103 +15,111 @@ namespace MijnCV_API.Test
 {
     public class SectionTest
     {
-        private readonly SectionsController _controller;
-        private readonly SectionServiceFake _service;
+        private readonly Mock<ISectionService> _service;
         public SectionTest()
         {
-            _service = new SectionServiceFake();
-            _controller = new SectionsController(_service);
+            _service = new Mock<ISectionService>();
         }
 
         [Fact]
         public void GetAllSectionsTest()
         {
-            var expected = new List<Section>()
-            {
-                new Section() { ID = 1, CV = "TestCV1", Layout = 1, PageID = 1, Paragraph="Dit is een test1", Position = 1, Title="Test1"},
-                new Section() { ID = 2, CV = "TestCV2", Layout = 2, PageID = 2, Paragraph="Dit is een test2", Position = 1, Title="Test2"},
-                new Section() { ID = 3, CV = "TestCV3", Layout = 1, PageID = 1, Paragraph="Dit is een test3", Position = 2, Title="Test3"},
-            };
-            var Result = _controller.GetSections();
+            var data = GetSectionsData();
+            _service.Setup(x => x.GetSections()).Returns(async () => data);
+            var controller = new SectionsController(_service.Object);
+
+            var Result = controller.GetSections();
 
             Assert.NotNull(Result);
             Assert.IsType<Task<ActionResult<IEnumerable<Section>>>>(Result);
-            Result.Result.Value.Should().Equals(expected);
+            Result.Result.Value.Should().Equals(data);
         }
 
         [Fact]
         public void GetSectionsTest()
         {
-            Section expected = new Section() { ID = 1, CV = "TestCV1", Layout = 1, PageID = 1, Paragraph = "Dit is een test1", Position = 1, Title = "Test1" };
-            var Result = _controller.GetSection(1);
+            var data = GetSectionsData();
+            _service.Setup(x => x.GetSection(1)).Returns(async () => data[0]);
+            var controller = new SectionsController(_service.Object);
+
+            var Result = controller.GetSection(1);
 
             Assert.NotNull(Result);
             Assert.IsType<Task<ActionResult<Section>>>(Result);
-            Result.Result.Value.Should().Equals(expected);
+            Result.Result.Value.Should().Equals(data[0]);
         }
 
         [Fact]
         public void DeleteSectionTest()
         {
-            var expected = new List<Section>()
-            {
-                new Section() { ID = 2, CV = "TestCV2", Layout = 2, PageID = 2, Paragraph="Dit is een test2", Position = 1, Title="Test2"},
-                new Section() { ID = 3, CV = "TestCV3", Layout = 1, PageID = 1, Paragraph="Dit is een test3", Position = 2, Title="Test3"},
-            };
+            var data = GetSectionsData();
+            _service.Setup(x => x.DeleteSection(1)).Returns(async () => true);
+            var controller = new SectionsController(_service.Object);
 
-            var result = _service.DeleteSection(1);
-            var test = _controller.GetSections();
+            var Result = controller.DeleteSection(1);
+            var Result2 = controller.GetSections();
 
-            Assert.IsType<Task<bool>>(result);
-            test.Result.Value.Should().Equals(expected);
+            Assert.NotNull(Result);
+            Result2.Result.Value.Should().Equals(data);
         }
 
         [Fact]
         public void PutSectionTest()
         {
-            Section expected = new Section() { ID = 1, CV = "TestCV1", Layout = 2, PageID = 1, Paragraph = "Dit is een test1", Position = 1, Title = "Test1Test" };
             Section section = new Section() { CV = "TestCV1", Layout = 2, PageID = 1, Paragraph = "Dit is een test1", Position = 1, Title = "Test1Test" };
 
-            var Result = _service.PutSection(1, section);
-            var Result2 = _controller.GetSection(1);
+            var data = GetSectionsData();
+            _service.Setup(x => x.PutSection(1, section)).Returns(async () => true);
+            var controller = new SectionsController(_service.Object);
+
+            var Result = controller.PutSection(1, section);
+            var Result2 = controller.GetSection(1);
 
             Assert.NotNull(Result);
-            Assert.IsType<Task<bool>>(Result);
-            Result2.Result.Value.Should().Equals(expected);
+            Result2.Result.Value.Should().Equals(data[0]);
         }
 
         [Fact]
         public void PostSectionTest()
         {
-            var expected = new List<Section>()
-            {
-                new Section() { ID = 1, CV = "TestCV1", Layout = 1, PageID = 1, Paragraph="Dit is een test1", Position = 1, Title="Test1"},
-                new Section() { ID = 2, CV = "TestCV2", Layout = 2, PageID = 2, Paragraph="Dit is een test2", Position = 1, Title="Test2"},
-                new Section() { ID = 3, CV = "TestCV3", Layout = 1, PageID = 1, Paragraph="Dit is een test3", Position = 2, Title="Test3"},
-                new Section() { ID = 4, CV = "TestCV1", Layout = 1, PageID = 1, Paragraph="Dit is een test4", Position = 2, Title="Test4"},
-            };
-
             Section section = new Section() { ID = 4, CV = "TestCV1", Layout = 1, PageID = 1, Paragraph = "Dit is een test4", Position = 2, Title = "Test4" };
 
-            var result = _service.PostSection(section);
-            var test = _controller.GetSections();
+            var data = GetSectionsData();
+            _service.Setup(x => x.PostSection(section)).Returns(async () => data);
+            var controller = new SectionsController(_service.Object);
 
-            Assert.IsType<Task<Section>>(result);
-            test.Result.Value.Should().Equals(expected);
+            var Result = controller.PostSection(section);
+            var Result2 = controller.GetSections();
+
+            Assert.NotNull(Result);
+            Assert.IsType<Task<ActionResult<Section>>>(Result);
+            Result2.Result.Value.Should().Equals(data);
         }
 
         [Fact]
         public void GetSectionsByCv()
         {
-            var expected = new List<Section>()
-            {
-                new Section() { ID = 1, CV = "TestCV1", Layout = 1, PageID = 1, Paragraph="Dit is een test1", Position = 1, Title="Test1"},
-            };
-            var Result = _controller.GetSectionsByCV("TestCV1");
+            var data = GetSectionsData();
+            _service.Setup(x => x.GetSectionsByCV("TestCV1")).Returns(async () => data);
+            var controller = new SectionsController(_service.Object);
+
+            var Result = controller.GetSectionsByCV("TestCV1");
 
             Assert.NotNull(Result);
             Assert.IsType<Task<ActionResult<IEnumerable<Section>>>>(Result);
-            Result.Result.Value.Should().Equals(expected);
+            Result.Result.Value.Should().Equals(data);
+        }
+
+        public List<Section> GetSectionsData()
+        {
+            List<Section> sections = new List<Section>()
+            {
+                new Section() { ID = 1, CV = "TestCV1", Layout = 1, PageID = 1, Paragraph="Dit is een test1", Position = 1, Title="Test1"},
+                new Section() { ID = 2, CV = "TestCV2", Layout = 2, PageID = 2, Paragraph="Dit is een test2", Position = 1, Title="Test2"},
+                new Section() { ID = 3, CV = "TestCV3", Layout = 1, PageID = 1, Paragraph="Dit is een test3", Position = 2, Title="Test3"},
+            };
+
+            return sections;
         }
     }
 }
